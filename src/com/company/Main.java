65d75c52c3e966a86b49ff6420f00bca21f8ab3d
bgravanda <bgravanda@student.bridgew.edu>
2021-09-16 10:54:02 -0400

    Draft1

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    var name = new ArrayList<String>();
+        var number = new ArrayList<Integer>();
+        var price = new ArrayList<Float>();
+        var filename = "Products.txt";
+        var filePath = Paths.get(filename);
+        var allLines = Files.readAllLines(filePath);
+        for (var line: allLines){
+            var splitLine = line.split(",");
+            //var splitLine2 = line.split(",");
+            name.add(splitLine[0]);
+            number.add(Integer.parseInt(splitLine[2]));
+            price.add(Float.parseFloat(splitLine[1]));
+
+            System.out.println(name);
+            System.out.println(number);
+            System.out.println(price);
+        }
+    }
+}

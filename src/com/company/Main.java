988a69f1802768cc92ecaca907d2e79299085c3e
bgravanda <bgravanda@student.bridgew.edu>
2021-09-21 11:12:00 -0400

    Final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,23 +12,29 @@ public class Main {
 	    var name = new ArrayList<String>();
         var number = new ArrayList<Integer>();
         var price = new ArrayList<Float>();
+        var reader= new Scanner(System.in);
         var filename = "Products.txt";
         var filePath = Paths.get(filename);
         var allLines = Files.readAllLines(filePath);
-        for (var line: allLines){
+        for (var line: allLines) {
             var splitLine = line.split(",");
-            var splitLine2 = line.split(",");
             name.add(splitLine[0]);
             number.add(Integer.parseInt(splitLine[2]));
             price.add(Float.parseFloat(splitLine[1]));
+        }
+            var userInput = "" ;
+            while(!userInput.equals("end")){
+                System.out.print("Enter item or 'end' when done:");
+                userInput=reader.nextLine();
+                if(userInput.equals("end")){
+                   break;
 
-            System.out.println(name);
-            System.out.println(number);
-            System.out.println(price);
-            var userInput = ;
-            while(userInput != "end"){
-
+                }
+                for(int i = 0; i< name.size(); i++){
+                    if(userInput.equals(name.get(i)))
+                    System.out.println(name.get(i)+" is "+price.get(i)+" and there are "+number.get(i)+" left");
+                }
             }
+
         }
     }
-}
